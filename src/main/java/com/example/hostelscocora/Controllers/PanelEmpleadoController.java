ea6ab787e4a5b2258b4278bdcdb8a90dad391c3f
anaSDuque <112828439+anaSDuque@users.noreply.github.com>
2023-08-27 01:27:54 -0500

    views4

---
+++ src/main/java/com/example/hostelscocora/Controllers/PanelEmpleadoController.java
@@ -0,0 +1,83 @@
+package com.example.hostelscocora.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PanelEmpleadoController {
+
+    @FXML
+    private TableView<?> TableCamas;
+
+    @FXML
+    private Button btnCrearCliente;
+
+    @FXML
+    private Button btnReservasCliente;
+
+    @FXML
+    private TableColumn<?, ?> colEsatdoHabitaciones;
+
+    @FXML
+    private TableColumn<?, ?> colEstadoCamas;
+
+    @FXML
+    private TableColumn<?, ?> colIdCama;
+
+    @FXML
+    private TableColumn<?, ?> colIdCliente;
+
+    @FXML
+    private TableColumn<?, ?> colIdHabitacion;
+
+    @FXML
+    private TableColumn<?, ?> colIdHabitaciones;
+
+    @FXML
+    private TableColumn<?, ?> colNombreCliente;
+
+    @FXML
+    private TableColumn<?, ?> colTipo;
+
+    @FXML
+    private TableColumn<?, ?> colTipoHabitaciones;
+
+    @FXML
+    private TableView<?> tableCliente;
+
+    @FXML
+    private TableView<?> tableHbabitacion;
+
+    @FXML
+    void onCrearClienteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEstadoCamaButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onIdCamaButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onIdHabitacionCamaButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReservasClienteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTipoCamaButtonClick(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/hostelscocora/Controllers/ReservaDeClienteController.java
@@ -0,0 +1,42 @@
+package com.example.hostelscocora.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ReservaDeClienteController {
+
+    @FXML
+    private Button btnAtras;
+
+    @FXML
+    private Button btnReservar;
+
+    @FXML
+    private TableColumn<?, ?> colEntradaReservas;
+
+    @FXML
+    private TableColumn<?, ?> colHabitacionReservas;
+
+    @FXML
+    private TableColumn<?, ?> colIdReservas;
+
+    @FXML
+    private TableColumn<?, ?> colSalidaReservas;
+
+    @FXML
+    private TableView<?> tableReservas;
+
+    @FXML
+    void onAtrasButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReservatButtonClick(ActionEvent event) {
+
+    }
+
+}
