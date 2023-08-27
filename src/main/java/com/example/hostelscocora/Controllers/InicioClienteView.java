eb75d162a821d769a23c57d33d5487e10a1a2c20
anaSDuque <112828439+anaSDuque@users.noreply.github.com>
2023-08-27 14:40:20 -0500

    views cliente

--- src/main/java/com/example/hostelscocora/Controllers/InicioClienteView.java
+++ src/main/java/com/example/hostelscocora/Controllers/InicioClienteView.java
@@ -1,4 +1,51 @@
 package com.example.hostelscocora.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class InicioClienteView {
+
+    @FXML
+    private Button bntEliminarReserva;
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    private Button btnReservar;
+
+    @FXML
+    private TableColumn<?, ?> colCamaExtraReservarCliente;
+
+    @FXML
+    private TableColumn<?, ?> colIDReservaReservarCliente;
+
+    @FXML
+    private TableColumn<?, ?> colUsuarioReservarCliente;
+
+    @FXML
+    private TableView<?> tableReservarCliente;
+
+    @FXML
+    private Label tfReservas;
+
+    @FXML
+    void onEliminarReservaButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRegresarButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReservarButtonClick(ActionEvent event) {
+
+    }
+
 }
