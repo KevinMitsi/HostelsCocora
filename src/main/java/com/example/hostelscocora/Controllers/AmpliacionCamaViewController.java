249b62d6faab5e3774602a4e3a3229509f6778f0
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2023-08-27 19:14:39 -0500

    Nuevas opciones en views

--- src/main/java/com/example/hostelscocora/Controllers/AmpliacionCamaViewController.java
+++ src/main/java/com/example/hostelscocora/Controllers/AmpliacionCamaViewController.java
@@ -1,12 +1,23 @@
 package com.example.hostelscocora.Controllers;
 
+import com.example.hostelscocora.HelloApplication;
+import com.example.hostelscocora.Model.Cama;
+import com.example.hostelscocora.Model.Empleado;
+import com.example.hostelscocora.Model.EstadoProducto;
+import com.example.hostelscocora.Model.TipoProducto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class AmpliacionCamaViewController {
+    HelloApplication main;
+    ModelFactoryController singleton = ModelFactoryController.getInstance();
+    Cama camaSeleccionada;
+    Empleado empleadoLoggeado;
 
     @FXML
     private Button btnAtras;
@@ -15,14 +26,14 @@ public class AmpliacionCamaViewController {
     private Button btnEnviar;
 
     @FXML
-    private ComboBox<?> cbxEstado;
+    private ComboBox<EstadoProducto> cbxEstado;
 
     @FXML
     private TextField tfIdHabitacion;
 
     @FXML
-    void onAtrasButtonClick(ActionEvent event) {
-
+    void onAtrasButtonClick(ActionEvent event) throws IOException {
+        main.abrirPanelEmpleado(empleadoLoggeado);
     }
 
     @FXML
@@ -30,4 +41,22 @@ public class AmpliacionCamaViewController {
 
     }
 
+    @FXML
+    void initialize(){
+        cbxEstado.getItems().add(EstadoProducto.DISPONIBLE);
+        cbxEstado.getItems().add(EstadoProducto.MANTENIMIENTO);
+        cbxEstado.getItems().add(EstadoProducto.OPERACION);
+    }
+
+    public void setMain(HelloApplication helloApplication) {
+        this.main=helloApplication;
+    }
+
+    public void setCamaSeleccionada(Cama camaSeleccionada) {
+        this.camaSeleccionada=camaSeleccionada;
+    }
+
+    public void setEmpleadoLoggeado(Empleado empleadoLogeado) {
+        this.empleadoLoggeado=empleadoLogeado;
+    }
 }
